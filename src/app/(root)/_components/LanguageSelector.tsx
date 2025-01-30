3196ce9d5bec8841664d76ca81d22b24db5012eb
Prajyot Shinde <prajyotshinde60@gmail.com>
2025-01-30 23:20:27 +0530

    Add new components: RunButton, EditorPanel, OutputPanel, ThemeSelector, LanguageSelector, Header, and HeaderProfileBtn; define types for themes, languages, and execution results

---
+++ src/app/(root)/_components/LanguageSelector.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LanguageSelector = ({hasAccess}: {hasAccess: boolean}) => {
+  console.log(hasAccess)
+  return (
+    <div>LanguageSelector</div>
+  )
+}
+
+export default LanguageSelector
