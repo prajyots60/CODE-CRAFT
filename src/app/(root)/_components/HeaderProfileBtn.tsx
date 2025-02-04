b0ed16a86adbb5f0d5e1ea41345b291fc64f3ae1
Prajyot Shinde <prajyotshinde60@gmail.com>
2025-02-04 17:48:47 +0530

    nothing to commit, working tree clean

--- src/app/(root)/_components/HeaderProfileBtn.tsx
+++ src/app/(root)/_components/HeaderProfileBtn.tsx
@@ -1,6 +1,6 @@
 'use client'
 import LoginButton from '@/components/LoginButton'
-import { SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { SignedOut, UserButton } from '@clerk/nextjs'
 import { User } from 'lucide-react'
 import React from 'react'
 
